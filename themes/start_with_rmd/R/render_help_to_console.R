823f59973820d4f10216a20d5f83e45850f04919
Marc Teunis <marc.teunis@hu.nl>
2019-12-04 11:48:54 +0100

    add demo for "Start with RMarkdown" workshop, 9 December 2019 - Marc Teunis

---
+++ themes/start_with_rmd/R/render_help_to_console.R
@@ -0,0 +1,24 @@
+## Render help documents to the Console
+
+## printing help to Rmd or console
+## from: https://www.r-bloggers.com/printing-r-help-files-in-the-console-or-in-knitr-documents/
+
+
+help_console <- function(topic, format=c("text", "html", "latex", "Rd"),
+                         lines=NULL, before=NULL, after=NULL) {  
+  format=match.arg(format)
+  if (!is.character(topic)) topic <- deparse(substitute(topic))
+  helpfile = utils:::.getHelpFile(help(topic))
+  
+  hs <- capture.output(switch(format, 
+                              text=tools:::Rd2txt(helpfile),
+                              html=tools:::Rd2HTML(helpfile),
+                              latex=tools:::Rd2latex(helpfile),
+                              Rd=tools:::prepare_Rd(helpfile)
+  )
+  )
+  if(!is.null(lines)) hs <- hs[lines]
+  hs <- c(before, hs, after)
+  cat(hs, sep="\n")
+  invisible(hs)
+}
